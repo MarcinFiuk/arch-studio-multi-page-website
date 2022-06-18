bc23d08dcd6a532aa3a69bb14e2b34c213e394cf
Marcin_Fi <fiukmarcinn@gmail.com>
2022-06-18 19:30:14 +0100

    refactor welcomeSection to be more reusable and use in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import useMatchMedia from './../hooks/useMatchMedia';
 import HeroSecondary from '../components/HeroSecondary';
+import SectionWithOnePhotoDisplayedAtDesktop from '../components/SectionWithOnePhotoDisplayedAtDesktop';
 
 function About() {
     const mobile = useMatchMedia('(max-width: 48rem)');
@@ -19,7 +20,22 @@ function About() {
                 title='Your team of professionals'
                 desc='Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.'
             />
-            {/* About short desc */}
+            <SectionWithOnePhotoDisplayedAtDesktop
+                photo='/assets/about/desktop/image-heritage.jpg'
+                alt='office windows'
+                photoWidth='540px'
+                photoHeight='568px'
+                changeDecorationElement={true}
+                title={
+                    <>
+                        Our <br />
+                        Heritage
+                    </>
+                }
+                desc1='Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.'
+                desc2='Specializing in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs.'
+                desc3='Our small team of world-class professionals provides input on every project.'
+            />
             {/* About Leaders */}
         </>
     );
